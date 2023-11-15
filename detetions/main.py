4b6e0fc957988863e439f0186138f722494a5309
wonjin <molra0307@naver.com>
2023-11-16 00:58:46 +0900

    11.16

--- detetions/main.py
+++ detetions/main.py
@@ -4,7 +4,7 @@ from lib.detetions.ObjectDetectionMaster import ObjectDetectionMaster
 bestEggs=""
 yolov8n="../../traing Model/weights/yolov8n.pt"
 
-sizeOfScreen=(640,640)
+sizeOfScreen=(960,640)
 
 ob=ObjectDetectionMaster(yolov8n,sizeOfScreen)
 def mainMaster(frame):
