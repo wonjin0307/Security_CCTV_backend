577877aca2bd6b76335bf0599ac63cc1ebf85719
wonjin <molra0307@naver.com>
2023-12-07 15:50:56 +0900

    12.07

--- detetions/main.py
+++ detetions/main.py
@@ -2,11 +2,12 @@ from lib.detetions.ObjectDetectionMaster import ObjectDetectionMaster
 
 
 bestEggs=""
-yolov8n="../../traing Model/weights/yolov8n.pt"
+yolov8n="../traingModel/weights/falldown_x.pt"
+yolo_best = "C:/Users/anyang/Desktop/project/backend/traingModel/weights/falldown_x.pt"
 
 sizeOfScreen=(960,640)
 
-ob=ObjectDetectionMaster(yolov8n,sizeOfScreen)
+ob=ObjectDetectionMaster(yolo_best,sizeOfScreen)
 def mainMaster(frame):
     frame,class_id=ob.detectionSupervision(frame)
     return frame,class_id
